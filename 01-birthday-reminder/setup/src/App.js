7f9b33b776e4030b6c40e938708bbeffe0bd30be
Levi <90584200+LeviBcodes@users.noreply.github.com>
2021-09-17 04:52:35 -0700

    completed the birthday reminder application

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,18 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people,setPeople] = useState(data)
+  return (
+  <main>
+    <section className='container'>
+      //get number of birthdays today using the length of the people array
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/>
+      //clear the list by setting the array to empty using setPeople function
+      <button onClick={()=> setPeople([])}>Clear All</button>
+    </section>
+  </main>
+  );
 }
 
 export default App;
