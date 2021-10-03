5c6d5ae12f4f04ee9dcb6d3e3a7384d23eba1157
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-02 22:23:33 -0700

    added categories manually. switching to a dynamic approach

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -5,7 +5,7 @@ const Menu = ({items}) => {
   return <div className="section-center">
     {items.map((menuItem) => {
       const {id,title,img,desc,price} = menuItem;
-      return <article key={id} className="menu-items">
+      return <article key={id} className="menu-item">
         <img src={img} alt={title} className="photo" />
         <div className="item-info">
           <header>
