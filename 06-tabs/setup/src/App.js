162ac939d034a5ceba717df4cf45e0cf9466dff5
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-03 16:06:08 -0700

    setting up fetch to load jobs

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [loading,setLoading] = useState(true);
+  const [jobs,setJobs] = useState([]);
+  const[value,setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+  }
   return <h2>tabs project setup</h2>
 }
 
