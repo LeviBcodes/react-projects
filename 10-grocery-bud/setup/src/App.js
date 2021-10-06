307f267c1db65519f54705aacfabf0727b391cb5
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-05 19:21:49 -0700

    setting up handleSubmit function to add new item to list

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -9,7 +9,17 @@ function App() {
   const [editID,setEditId] = useState(null);
   const [alert,setAlert] = useState({show:false,msg:'',type:''});
   const handleSubmit = (e) => {
-    e.preventDefault()
+    if(!name){
+
+    }
+    else if(name && isEditing) {
+
+    }
+    else {
+      const newItem = {id: new Date().getTime().toString(),title:name};
+      setList([...list,newItem]);
+      setName('');
+    }
   }
 
   return <section className="section-center">
