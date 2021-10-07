621d2cd0fa7efee31d8e8c14982dcf5c634ee509
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-07 00:33:43 -0700

    adding functionality to delete single item

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -32,6 +32,11 @@ function App() {
     setList([])
   }
 
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed')
+    setList(list.filter((item)=> item.id !== id))
+  }
+
   return <section className="section-center">
     <form action="" onSubmit={(handleSubmit)}>
       {alert.show && <Alert {...alert} removeAlert={showAlert()} />}
@@ -46,7 +51,7 @@ function App() {
     </form>
     {list.length>0 &&(
     <div className="grocery-container">
-      <List items={list} />
+      <List items={list} removeItem={removeItem} />
       <button className="clear-btn" onClick={clearList}>
 
       </button>
