be3793e9264ff4ca36b3aa82e4abe1ce53e16765
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-10 00:59:57 -0700

    adding functionality to open and close the modal.

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -3,8 +3,36 @@ import React, { useState, useContext } from 'react'
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+
     return (
-        <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+        <AppContext.Provider 
+            value={{
+                isModalOpen,
+                isSidebarOpen,
+                openModal,
+                openSidebar,
+                closeModal,
+                closeSidebar
+            }}>{children}
+        </AppContext.Provider>
     )
 }
 
