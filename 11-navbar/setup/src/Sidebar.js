3cb8aade37633b8dbfae049aeba0a67c691552d2
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-09 22:11:44 -0700

    adding links via foreach to itterate through data

--- 11-navbar/setup/src/Sidebar.js
+++ 11-navbar/setup/src/Sidebar.js
@@ -1,23 +1,21 @@
 import React from 'react'
+import { links, social } from './data'
+
 
 const Sidebar = () => {
     return (
         <div className="links-container show-container">
             <ul className="links">
-            <li>
-                <a href="#">home</a>
-            </li>
-            <li>
-                <a href="#">about</a>
-            </li>
-            <li>
-                <a href="#">contact</a>
-            </li>
-            <li>
-                <a href="#">products</a>
-            </li>
+                {links.map((link) => {
+                    const {id,url,text} = link;
+                    return (
+                    <li key={id}>
+                        <a href={url}>{text}</a>
+                    </li>
+                    )
+                })}
             </ul>
-      </div>
+        </div>
     )
 }
 
