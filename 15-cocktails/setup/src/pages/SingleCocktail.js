9134387d476be4895af192d756e6d36d00ff4a4f
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-16 00:28:58 -0600

    added single cocktail details to single cocktails component

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -47,9 +47,9 @@ const SingleCocktail = () => {
     }
     getCocktail()
   },[id])
-  if(loading) {
-    return <Loading />
-  }
+  // if(loading) {
+  //   return <Loading />
+  // }
   if(!cocktail) {
     return (
       <h2 className="section-title">no cocktails</h2>
@@ -57,8 +57,40 @@ const SingleCocktail = () => {
   }
   const {name,image,category,info,glass,instructions,ingredients} = cocktail
   return (
-    <section>
+    <section className="section cocktail-section">
+      <Link to="/" className="btn btn-primary"> back home</Link>
       <h2 className="section-title">{name}</h2>
+      <div className="drink">
+        <img src={image} alt={name} />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name: </span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category: </span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info: </span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass: </span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">instructions: </span>
+            {instructions}
+          </p>
+          <p>
+            <span className="drink-data">ingredients: </span>
+            {ingredients.map((item,index) => {
+              return item? <span key={index}>{item}</span> : null
+            })}
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
