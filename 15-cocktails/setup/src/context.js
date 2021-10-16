7653d91796acbb431822ad0eed385172365bf0ad
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-15 23:07:11 -0600

    handling errors for invalid search terms

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -9,14 +9,23 @@ const AppProvider = ({ children }) => {
   const [searchTerm,setSearchTerm] = useState('a')
   const [cocktails,setCocktails] = useState([])
 
-  const fetchDrinks = asynch () => {
+  const fetchDrinks = async () => {
     setLoading(true)
     try {
       const response = await fetch(`${url}${searchTerm}`)
       const data = await response.json()
+      const {drinks} = data;
+      if(drinks) {
+
+      }
+      else {
+        setCocktails([])
+      }
+      setLoading(false)
       
     } catch (error) {
       console.log(error)
+      setLoading(false)
     }
   }
 
