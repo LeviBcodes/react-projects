f40d6fde3aaf1d1e4baf02d09d575b145de3be82
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-15 22:32:09 -0600

    setting up error page

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In voluptatum quo numquam sequi adipisci assumenda, minima voluptatibus, necessitatibus amet tenetur odit illo doloremque a ut dignissimos, neque nobis aliquid cupiditate.
+    </p>
+    </section>
   )
 }
 
